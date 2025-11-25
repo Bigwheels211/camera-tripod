c7105e75a65caf2ce882be024a5dd2894d7f6de3
Bigwheels211 <124935742+Bigwheels211@users.noreply.github.com>
2025-11-25 10:34:13 -0600

    Update motorControl.py

--- motorControl.py
+++ motorControl.py
@@ -1,1 +1,8 @@
+import gpiozero
 
+tiltMotorForwardPin = 0
+tiltMotorBackwardPin = 0
+tiltMotorActivatePin = 0
+rotationMotorForwardPin = 0
+rotationMotorBackwardPin = 0
+rotationMotorActivatePin = 0
