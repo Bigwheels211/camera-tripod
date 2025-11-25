bd77ac18b3439d5688d5ff0851efd772c351f024
Bigwheels211 <124935742+Bigwheels211@users.noreply.github.com>
2025-11-25 11:18:38 -0600

    adding motorControl logic

--- motorControl.py
+++ motorControl.py
@@ -1,8 +1,34 @@
 import gpiozero
-
+import threading
+import time
 tiltMotorForwardPin = 0
 tiltMotorBackwardPin = 0
 tiltMotorActivatePin = 0
 rotationMotorForwardPin = 0
 rotationMotorBackwardPin = 0
 rotationMotorActivatePin = 0
+
+class motorController:
+    tiltMotor = Motor(tiltMotorForwardPin, tiltMotorBackwardPin, tiltMotorActivatePin)
+    rotationMotor = Motor(rotationMotorForwardPin, rotationMotorBackwardPin, rotationMotorActivatePin)
+    def moveX(xVector):
+        if (xVector < 0):
+            rotationMotor.reverse()
+            sleep(0.03)
+            rotationMotor.stop()
+        if (xVector > 0):
+            rotationMotor.forward()
+            sleep(0.03)
+            rotationMotor.stop()
+    def moveY(yVector):
+        if (yVector < 0):
+            tiltMotor.reverse()
+            sleep(0.03)
+            tiltMotor.stop()
+        if (yVector > 0):
+            tiltMotor.forward()
+            sleep(0.03)
+            tiltMotor.stop()
+    def move(xVector, yVector):
+        moveX(xVector)
+        moveY(yVector)
