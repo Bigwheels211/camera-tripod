b37a2396e72f9500de194940d4126e5cbccb0a80
Bigwheels211 <tyler.jackson.martinez@gmail.com>
2025-11-25 21:57:15 -0600

    FINALLY FIXED PIGPIO!!!!

--- motorControl.py
+++ motorControl.py
@@ -1,17 +1,17 @@
 from gpiozero import Motor, Device
-#from gpiozero.pins.rpigpio import PiGPIOFactory
+from gpiozero.pins.pigpio import PiGPIOFactory
 import threading
 import time
 
-#Device.pin_factory = PiGPIOFactory()
+Device.pin_factory = PiGPIOFactory()
 tiltMotorForwardPin = 12
 tiltMotorBackwardPin = 13
 rotationMotorForwardPin = 18
 rotationMotorBackwardPin = 19
 
 class motorController:
-    tiltMotor = Motor(tiltMotorForwardPin, tiltMotorBackwardPin, pwm=False)
-    rotationMotor = Motor(rotationMotorForwardPin, rotationMotorBackwardPin, pwm=False)
+    tiltMotor = Motor(tiltMotorForwardPin, tiltMotorBackwardPin)
+    rotationMotor = Motor(rotationMotorForwardPin, rotationMotorBackwardPin)
     def moveX(self, xVector):
         if (xVector < 0):
             self.rotationMotor.reverse()
