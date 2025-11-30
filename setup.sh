be811de227291dd7294b81a6d0c5218c7029e0c8
Bigwheels211 <124935742+Bigwheels211@users.noreply.github.com>
2025-11-30 06:34:14 -0600

    Update virtual environment activation path in setup.sh

--- setup.sh
+++ setup.sh
@@ -81,7 +81,7 @@ fi
 #-------------------------------
 # 5. Test Picamera2 import
 #-------------------------------
-source /home/(whoami)/.venv/bin/activate
+source ~/camera-tripod/.venv/bin/activate
 echo "Testing Picamera2..."
 python3 - << 'EOF'
 try:
@@ -96,7 +96,7 @@ EOF
 #-------------------------------
 # 6. Test pigpio import
 #-------------------------------
-source /home/(whoami)/.venv/bin/activate
+source ~/camera-tripod/.venv/bin/activate
 echo "Testing pigpio..."
 python3 - << 'EOF'
 try:
